b50c6daee3d0e48135f87c3a43b5fcf0db6a5048
amarcinkowski <andrzej.marcinkowski@gmail.com>
2016-11-23 23:06:51 +0100

    add logo

--- src/main/java/io/github/amarcinkowski/controller/BookController.java
+++ src/main/java/io/github/amarcinkowski/controller/BookController.java
@@ -7,7 +7,6 @@ import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
