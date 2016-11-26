f5eaf8e478cef90de9b7397db10a06354cff1848
amarcinkowski <andrzej.marcinkowski@gmail.com>
2016-11-26 12:11:39 +0100

    dependency for running in intellij idea

--- src/main/java/io/github/amarcinkowski/BooksApplication.java
+++ src/main/java/io/github/amarcinkowski/BooksApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BooksApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(BooksApplication.class, args);
 	}
+
 }
